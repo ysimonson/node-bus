172c0ee68ff7ba2ef5e6b75ba0a5577fbc5d7077
Yusuf Simonson <simonson@gmail.com>
2010-06-02 21:36:41 -0400

    Removed buggy index implementation

--- examples/server.js
+++ examples/server.js
@@ -16,64 +16,26 @@ var server = http.createServer().addListener('request', function (req, res) {
 server.listen(8080);
 
 var listener = io.listen(server, {
-    subscriptions: {},
-    subscriberIndex: {},
-    
-    addListener: function(event, client) {
-        var indexContainer = this.subscriberIndex[client.sessionId];
-        if(!indexContainer) this.subscriberIndex[client.sessionId] = indexContainer = {};
-        if(indexContainer[event] != undefined) return false;
-        
-        var container = this.subscriptions[event];
-        if(!container) this.subscriptions[event] = container = [];
-        container.push(client);
-        
-        indexContainer[event] = container.length - 1;
-        return true;
-    },
-    
-    removeListener: function(event, client) {
-        #bug caused by indices going stale
-        var indexContainer = this.subscriberIndex[client.sessionId];
-        if(!indexContainer) return false;
-        
-        var index = indexContainer[event];
-        if(index == undefined) return false;
-        
-        var container = this.subscriptions[event];
-        if(!container) return false;
-        
-        container.splice(index, 1);
-        if(container.length == 0) delete this.subscriptions[event];
-        
-        delete this.indexContainer[event];
-        if(indexContainer.length == 0) delete this.subscriberIndex[client.sessionId];
-        
-        return true;
-    },
-    
     onClientMessage: function(message, client) {
         var json = null;
         try {
-            json = JSON.parse(message);
             sys.puts('received: ' + message);
+            json = JSON.parse(message);
         } catch(e) {
             //TODO: give an error message
+            sys.puts('error');
             return;
         }
         
         if(json instanceof Array) {
-            var container = this.subscriptions[json[0]];
-            for(var subscribers in container) {
-                subscribers.send(json);
-            }
+            listener.broadcast(message);
         } else if(typeof json == 'object') {
             var messageType = json['type'];
             
             if(messageType == 'listen') {
-                this.addListener(json['event'], client);
+                //TODO
             } else if(messageType == 'unlisten') {
-                this.removeListener(json['event'], client);
+                //TODO
             } else {
                 //TODO: given an error message
                 return;
